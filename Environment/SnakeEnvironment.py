9ef4bfcb2b68a2538d38ab21ed83925c85e053f9
Allan2Fernandes <allan2fernandes@hotmail.com>
2023-08-04 07:22:35 +0200

    Adjusting reward system

--- Environment/SnakeEnvironment.py
+++ Environment/SnakeEnvironment.py
@@ -1,59 +1,118 @@
 import random
 import sys
-from threading import Thread
 import pygame
-import time
+import numpy as np
+from Environment.DQN import DQN
+import torch
 
 class SnakeEnvironment:
-    def __init__(self, x_blocks, y_blocks, block_size):
+    def __init__(self, x_blocks, y_blocks, block_size, device, num_episodes, max_time_steps, batch_size):
+        self.num_episodes = num_episodes
+        self.max_time_steps = max_time_steps
+        self.device = device
+        self.batch_size = batch_size
+        self.food_position = None
+        self.is_running = None
+        self.current_direction = None
+        self.snake_body = None
         self.block_size = block_size
         self.x_blocks = x_blocks
         self.y_blocks = y_blocks
+        self.action_size = 3
+        self.state_size = self.x_blocks*self.y_blocks + 2
+        self.dqn = DQN(action_size=self.action_size, state_size=self.state_size, device=device)
         pygame.init()
         # create the display surface object of specific dimension.
         self.window = pygame.display.set_mode((x_blocks * self.block_size, y_blocks * self.block_size))
-        self.snake_body = [] # List of tuples
-        self.observation = None
+
+        self.fps = pygame.time.Clock()
+        self.game_loop(self.window)
+        pass
+
+    def expand_state_dims(self, state):
+        state = torch.tensor(state)
+        state = torch.unsqueeze(state, dim=0)
+        state = state.to(self.device)
+        return state
+
+
+    def reset(self):
+        self.snake_body = []  # List of tuples
         self.current_direction = 0
         self.generate_snake_starting_positions_directions()
-        self.fps = pygame.time.Clock()
         self.is_running = True
         self.food_position = self.generate_food()
-        self.game_loop(self.window)
-
-
+        return self.get_state(False)
 
     def game_loop(self, window):
-        while self.is_running:
+        num_steps_completed = 0
+        for episode in range(self.num_episodes):
+            init_state = self.reset()
+            state = self.expand_state_dims(init_state)
+            done = False
+            time_step = 0
+            episode_reward = 0
+            time_steps_without_reward = 0
+            while not done:
+                num_steps_completed += 1
+                if num_steps_completed % self.dqn.update_rate == 0:
+                    self.dqn.update_target_network()
+                # Draws the surface object to the screen.
+                pygame.display.update()
+                self.draw_game_state(window=window)
+                for event in pygame.event.get():
+                    # if event.type == pygame.KEYUP:
+                    #     if event.key == pygame.K_0:
+                    #         action = 0
+                    #     elif event.key == pygame.K_1:
+                    #         action = 1
+                    #     elif event.key == pygame.K_2:
+                    #         action = 2
+                    #     else:
+                    #         action = 2
+                    #     next_state, reward, done = self.env_step(action)
 
-            # Draws the surface object to the screen.
-            pygame.display.update()
-            self.draw_game_state(window=window)
-
-
-            for event in pygame.event.get():
-                # Manually selecting actions
-                if event.type == pygame.KEYUP:
-                    # Handle the specific key(s) you want to detect when they are released
-                    if event.key == pygame.K_0:
-                        action = 0
-                    elif event.key == pygame.K_1:
-                        action = 1
+                    #next_state = self.expand_state_dims(next_state)
+                    #self.dqn.store_transition(state, action, reward, next_state, done, 0)
+                    # Store transition
+                    #state = next_state
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+                        break
                         pass
-                    elif event.key == pygame.K_2:
-                        action = 2
-                        pass
-                    self.env_step(action)
-
-
-                if event.type == pygame.QUIT:
-                    self.is_running = False
-                    break
                     pass
 
-            #self.fps.tick(5)  # Adjust the speed of the game here (higher value -> faster)
-        pygame.quit()
-        sys.exit()
+                action = self.dqn.epsilon_greedy(state=state)
+                next_state, reward, done = self.env_step(action)
+                episode_reward += reward
+                if reward == 0:
+                    time_steps_without_reward += 1
+                else:
+                    time_steps_without_reward = 0
+                    pass
+
+                next_state = self.expand_state_dims(next_state)
+                self.dqn.store_transition(state, action, reward, next_state, done)
+                # Store transition
+                state = next_state
+                self.fps.tick(60)
+                if done:
+                    print("Episode", episode, episode_reward, "Snake Length", len(self.snake_body))
+                    pass
+
+                if len(self.dqn.replay_buffer) > self.batch_size:
+                    self.dqn.train_double_DQN(batch_size=self.batch_size)
+                    pass
+                if time_steps_without_reward >= self.max_time_steps:
+                    print("truncated")
+                    done = True
+                pass
+            self.dqn.decay_epsilon()
+
+            pass
+        pass
+
 
     def env_step(self, action):
         if action == 0:
@@ -69,10 +128,12 @@ class SnakeEnvironment:
         elif action == 2: #Do nothing
             pass
 
-        self.move_snake()
-        pass
+        next_state, reward, done = self.move_snake()
+        return next_state, reward, done
 
     def move_snake(self):
+        reward = 0
+        done = False
         # Find the new snake head position
 
         # Find the new snake head position
@@ -88,26 +149,45 @@ class SnakeEnvironment:
         elif self.current_direction == 3:  # West
             head_x -= 1
 
-
         # Create a new head position and add it to the front of the list
         new_head = (head_x, head_y)
         if new_head == self.food_position:
-            print("Eating food")
             self.snake_body.insert(0, new_head)
-            self.food_position = self.generate_food()
+            # Check if the game has ended and if not, generate food
+            if len(self.snake_body) == self.x_blocks*self.y_blocks:
+                done = True
+                reward += 20
+            else:
+                self.food_position = self.generate_food()
+                pass
+            reward += 5
         else:
-            self.snake_body.insert(0, new_head)
+            reward -= 0.01
             # Remove the last element to maintain the snake length
             self.snake_body.pop()
+            # Check if the snake eats itself or meets edge
+            if (head_x, head_y) in self.snake_body:
+                #print("eating tail")
+                reward -= 1
+                done = True
+            elif head_x == -1 or head_x == self.x_blocks or head_y == -1 or head_y == self.y_blocks:
+                #print("edge detected")
+                reward -= 1
+                done = True
+                pass
+
+            self.snake_body.insert(0, new_head)
             pass
+        next_state = self.get_state(done)
+        return next_state, reward, done
 
 
     def generate_snake_starting_positions_directions(self):
-        head_x = random.randint(0, self.x_blocks-1)
-        head_y = random.randint(0, self.y_blocks-2)
+        head_x = random.randint(1, self.x_blocks-1)
+        head_y = random.randint(1, self.y_blocks-2)
         self.snake_body.append((head_x, head_y))
         self.snake_body.append((head_x, head_y+1))
-        self.current_direction = random.randint(0, 3)
+        self.current_direction = 0
 
     def generate_food(self):
         all_possible_coordinates = set(
@@ -121,6 +201,28 @@ class SnakeEnvironment:
         food_x, food_y = random.choice(list(empty_coordinates))
 
         return food_x, food_y
+
+    def get_state(self, done):
+        # Create a 0s matrix the size of the board
+        game_state = np.zeros((self.x_blocks, self.y_blocks))
+        game_state = game_state.astype(np.float32)
+        if done:
+            game_state = game_state.flatten()
+            game_state = np.pad(game_state, (0, 2), 'constant')
+            return game_state
+        for x,y in self.snake_body:
+            game_state[y][x] = -1
+            pass
+
+        game_state[self.snake_body[0][1]][self.snake_body[0][0]] = 1
+        game_state[self.food_position[1]][self.food_position[0]] = 3
+        game_state = game_state.flatten()
+        game_state = np.pad(game_state, (0, 2), 'constant')
+        game_state[-2] = (self.snake_body[0][0] - self.food_position[0])/self.x_blocks
+        game_state[-1] = (self.snake_body[0][1] - self.food_position[1])/self.y_blocks
+
+        return game_state
+
 
     def draw_game_state(self, window):
         # Fill the screen with white color
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import torch
 from Environment.SnakeEnvironment import SnakeEnvironment
 
-env = SnakeEnvironment(5, 5, 100)
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+#device = torch.device('cpu')
+num_episodes = 5000
+max_time_steps = 500
+batch_size = 32
+env = SnakeEnvironment(5, 5, 40, device=device, num_episodes=num_episodes, max_time_steps=max_time_steps, batch_size=batch_size)
+
 
